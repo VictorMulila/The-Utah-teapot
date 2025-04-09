23c10f30c57850381fda720779b4d9d4140424a7
VictorMulila <146417375+VictorMulila@users.noreply.github.com>
2025-04-09 23:30:37 +0300

    Add files via upload

---
+++ pot.cpp
@@ -0,0 +1,30 @@
+#include <GL/glut.h>
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+    glColor3f(1.0, 1.0, 1.0);
+    glutWireTeapot(1.0);
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h) {
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, (float)w / h, 0.1, 100);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Utah Teapot (GLUT)");
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glEnable(GL_DEPTH_TEST);
+    glutMainLoop();
+    return 0;
+}
